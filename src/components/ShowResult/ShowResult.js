45ca06b754a9a60ca28d3ebb97502c6c4530eed4
lukasz.marciniak <lukasz.marciniak@thomsonreuters.com>
2019-06-28 06:45:16 +0200

    add info when cannot find any words with specifed conditions

--- src/components/ShowResult/ShowResult.js
+++ src/components/ShowResult/ShowResult.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import './ShowResult.css';
-import { Table } from 'react-bootstrap';
+import { Table, Alert } from 'react-bootstrap';
 
 const ShowResult = ({ data }) => (
-  <Table striped bordered hover>
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Word</th>
-        <th>Count</th>
-      </tr>
-    </thead>
-    <tbody>
-      {data.reverse().map((val, index) => (
-        <tr key={index}>
-          <td>{index + 1}</td>
-          <td>{val[0]}</td>
-          <td>{val[1]}</td>
-        </tr>
-      ))}
-    </tbody>
-  </Table>
+  <>
+    {data.length === 0 ? (
+      <Alert variant={'warning'}>
+        <strong style={{ marginRight: '10px' }}>Warning!</strong>
+        There are no words that meet the conditions
+      </Alert>
+    ) : (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Word</th>
+            <th>Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.reverse().map((val, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{val[0]}</td>
+              <td>{val[1]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    )}
+  </>
 );
 
 export default ShowResult;
